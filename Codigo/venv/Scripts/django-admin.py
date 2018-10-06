39e95e01e8f3f1f2d5984ee29e6c4f992bec7389
ChemaLopez <joselo01@ucm.es>
2018-10-06 17:17:56 +0200

    Añadido inicio prototipo Basado en python con el framework DJango

---
+++ Codigo/venv/Scripts/django-admin.py
@@ -0,0 +1,5 @@
+#!C:\Users\Chema Lopez\Documents\GitHub\TFG-1718-PictoTexto\Codigo\venv\Scripts\python.exe
+from django.core import management
+
+if __name__ == "__main__":
+    management.execute_from_command_line()
