062aeee17b7e777f3f03e992364f89e9c73d12b5
ChemaLopez <joselo01@ucm.es>
2019-01-26 18:14:48 +0100

    Arquitecture change

--- Codigo/pict2Text/translateService/views.py
+++ Codigo/pict2Text/translateService/views.py
@@ -1,6 +1,4 @@
-from django.shortcuts import render
-from django.http import HttpResponse
-from django.template import loader
+from django.http import JsonResponse
 import json
 
 import requests
@@ -8,14 +6,15 @@ import requests
 
 def getTranslate(request):
     headers = {'content-type': 'application/json', 'connection': 'keep-alive', 'Accept': 'application/json'}
-    context={}
+    response={}
+    print(request.POST)
     data = {"subject":request.POST['subject'], "verb":request.POST['verb'], "object":request.POST['object']};
     r = requests.post("http://127.0.0.1:8080/apiNLG/createSimplePhrase", data=json.dumps(data), headers=headers)
     if r.status_code == 200:
-        context = {
+        response = {
                 'translateResult': r.text
         }
     else:
-        print(r.status_code)
+        response={'status':r.status_code}
 
-    return render(request,'index.html', context)
+    return JsonResponse(response)
