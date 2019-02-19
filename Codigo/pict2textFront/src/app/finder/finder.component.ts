049af04afbf5e428e0d4d8b5d708a1240d2725a0
ChemaLopez <joselo01@ucm.es>
2019-02-19 20:48:35 +0100

    DataBinding and Drag order implemented

--- Codigo/pict2textFront/src/app/finder/finder.component.ts
+++ Codigo/pict2textFront/src/app/finder/finder.component.ts
@@ -1,18 +1,25 @@
-import { Component } from '@angular/core';
+import { Component, Input } from '@angular/core';
 import {FinderService} from 'src/app/finder/service/finder-service.service'
 import {Picto} from 'src/app/finder/transformer/picto'
 import { ProxyService } from '../utils/proxy/proxy-service.service';
 import {FinderTransformer} from 'src/app/finder/transformer/finder-transformer.transformer'
+import {NgbCarouselConfig} from '@ng-bootstrap/ng-bootstrap';
+
 
 @Component({
   selector: 'app-finder',
   templateUrl: './assets/finder.component.html',
-  providers: [FinderService,FinderTransformer, ProxyService],
+  providers: [FinderService,FinderTransformer, ProxyService,NgbCarouselConfig],
   styleUrls: ['./assets/finder.component.css']
 })
-export class FinderComponent  {
-  myPicto:Picto[]
-  constructor( private pictoService : FinderService) {}
+export class FinderComponent {
+  myPicto:Picto[];
+  @Input() pictoPhrase:Picto[];
+  
+  
+  constructor( private pictoService : FinderService, config: NgbCarouselConfig) {
+    config.interval=0;
+  }
 
    getPictosByName(name:string){
     this.pictoService.getPictByName(name).then(this.getPictoSucces.bind(this), this.getPictoError);
@@ -21,6 +28,10 @@ export class FinderComponent  {
    getPictoSucces(data){
     this.myPicto=data;
   }
+
+    addPicto(picto : Picto){
+      this.pictoPhrase.push(picto);
+    }
 
    getPictoError(){
     console.log("todo mal en el componente")
--- Codigo/pict2textFront/src/app/translator/translator.component.ts
+++ Codigo/pict2textFront/src/app/translator/translator.component.ts
@@ -1,6 +1,8 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, Input } from '@angular/core';
 import { ProxyService } from '../utils/proxy/proxy-service.service';
 import { TranslatorService } from 'src/app/translator/service/translator.service';
+import {CdkDragDrop, moveItemInArray} from '@angular/cdk/drag-drop';
+import {Picto} from 'src/app/finder/transformer/picto'
 
 @Component({
   selector: 'app-translator',
@@ -13,11 +15,15 @@ export class TranslatorComponent {
 
   phrase: string;
   finalPhrase: string;
+  @Input() pictoPhrase:Picto[];
   constructor(private translatorService: TranslatorService) { }
 
   getTranslate() {
-    let words;
-    words = this.phrase.split(' ');
+    let words=[], img;
+    
+    for(img in this.pictoPhrase ){
+        words.push(img.keyword)
+    }
     this.translatorService.getPictogramTranslate(words).then(this.getTranslateSucces.bind(this), this.getTranslateError);
   }
 
@@ -29,6 +35,12 @@ export class TranslatorComponent {
   getTranslateError(data) {
     console.log(data);
     console.log('todo mal en el componente');
+  }
+
+
+
+  drop(event: CdkDragDrop<string[]>) {
+    moveItemInArray(this.pictoPhrase, event.previousIndex, event.currentIndex);
   }
 
 }
