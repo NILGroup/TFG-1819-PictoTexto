e7b9729060bb4c23870410011bc833024830fab4
ChemaLopez <joselo01@ucm.es>
2019-04-10 21:56:22 +0200

    Creation new chapter and some corrections on Documentation

--- Codigo/pict2Text/pict2Text/pictoTranslateService/tests/tests.py
+++ Codigo/pict2Text/pict2Text/pictoTranslateService/tests/tests.py
@@ -5,8 +5,10 @@ from django.test import Client
 from django.urls import reverse
 
 class TranslateWebServiceTest(TestCase):
+
     def getWordAttrsTest(self):
         c = Client();
         response = c.post(reverse('translate/getWordAttrs'), {'paella','perro','comer'})
+        print("JEllo")
         self.assertEquals(response.status_code,200)
         pass
