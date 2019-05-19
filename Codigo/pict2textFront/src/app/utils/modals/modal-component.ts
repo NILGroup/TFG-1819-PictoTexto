17ab0704d9449a5df02d544eafa8cd614adf3190
ChemaLopez <joselo01@ucm.es>
2019-05-19 13:40:24 +0200

    Added filter to filters

--- Codigo/pict2textFront/src/app/utils/modals/modal-component.ts
+++ Codigo/pict2textFront/src/app/utils/modals/modal-component.ts
@@ -11,6 +11,21 @@ export class ModalComponent  implements OnInit {
   constructor( public activeModal: NgbActiveModal ) {}
 
   ngOnInit() {
+
+    if(this.errorText="OK"){
+      switch(this.errorCode){
+
+        case 404:
+            this.errorText ="Recurso no encontrado"
+            break;
+        case 500:
+            this.errorText="El servicio no está disponible, intentelo más tarde";
+            break;
+        default: 
+          this.errorText ="Ha ocurrido un error, intentelo más tarde"
+      }
+
+    }
   }
 
 
